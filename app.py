1d46d6917fc5cdae0e88e681157b903c437b8063
lokie2003 <141050042+lokie2003@users.noreply.github.com>
2023-11-01 11:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
@@ -8,10 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 model = load_model('StockPrice.h5')  # Replace 'your_model.h5' with the actual path to your model
 
 # Define a function to make predictions
-def make_predictions(start_date, num_days):
-    # Initialize the input sequence with data from 'start_date'
-    input_sequence = df[df['date'] >= start_date]['close'].values.tolist()
-    
+def make_predictions(input_sequence, num_days):
     # Scaling the input sequence
     scaler = MinMaxScaler(feature_range=(0, 1))
     input_sequence = scaler.fit_transform(np.array(input_sequence).reshape(-1, 1))
@@ -35,37 +31,25 @@ def make_predictions(start_date, num_days):
     
     return predictions
 
-# Load the data
-df = pd.read_csv('AAPL.csv')
-
 # Streamlit app
 st.title('Stock Price Prediction App')
 
 # Sidebar with user input
 st.sidebar.header('User Input')
-start_date = st.sidebar.date_input('Select a start date', pd.to_datetime('2023-01-01'))
+input_sequence = st.sidebar.text_area('Enter historical stock prices (comma-separated)', value='1.0, 2.0, 3.0, 4.0, 5.0')
+input_sequence = [float(x.strip()) for x in input_sequence.split(',')]
 num_days = st.sidebar.slider('Number of Days to Predict', 1, 30, 10)
 
 # Main content
-st.write(f"Predicting {num_days} days of stock prices starting from {start_date}")
+st.write(f"Predicting {num_days} days of stock prices based on user input")
 
 # Make predictions
 if st.button('Make Predictions'):
-    predictions = make_predictions(start_date, num_days)
+    predictions = make_predictions(input_sequence, num_days)
     st.write(predictions)
-
-# Data table
-st.subheader('Data Table')
-st.write(df.tail())
-
-# Plot the historical data
-st.subheader('Historical Data')
-st.line_chart(df[df['date'] >= start_date]['close'])
 
 # Plot the predicted prices
 st.subheader('Predicted Prices')
 if st.button('Plot Predicted Prices'):
-    predicted_dates = pd.date_range(start=start_date, periods=num_days)
-    predicted_data = pd.DataFrame({'date': predicted_dates, 'predicted_price': predictions})
-    st.line_chart(predicted_data)
+    st.line_chart(predictions)
 
