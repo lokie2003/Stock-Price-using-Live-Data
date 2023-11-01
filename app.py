2f3f7e86a536ccf69ace3e6bc66bd31577c547f0
lokie2003 <141050042+lokie2003@users.noreply.github.com>
2023-11-01 12:22:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
+import joblib
 
 # Load the pre-trained LSTM model
 model = load_model("StockPricefinal.h5")
